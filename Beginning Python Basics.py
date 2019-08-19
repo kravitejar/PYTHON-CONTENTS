97583fcc383cc2d47324ad77389c257f26632252
kravitejar <54026769+kravitejar@users.noreply.github.com>
2019-08-19 17:59:10 +0530

    Add files via upload

---
+++ Beginning Python Basics.py
@@ -0,0 +1,54 @@
+#Tag in python is used as comment line
+
+#PYTHON BASICS
+
+a = print("Hello World")
+print(a)
+
+print(1)
+
+#Pass multiple Strings
+print("Hello","World")
+
+print("Hello","World","Welcome","To","Python",1)
+
+print("Hello","World","Welcome","To","Python", sep = "#")
+
+print ("Hello","World", sep = "# #",end ="!")
+
+
+#NO VALUES TO PRINT STATEMENT,PRINTS OUT A LINE
+print()
+print("###############")
+print()
+print()
+print("###############")
+print()
+print()
+print()
+print()
+print("###############")
+
+
+#%S-string %d - integers %f - float or decima values
+
+
+#C-Style Formatters
+a ="AKSHEY"
+b =17
+c =82.345
+
+print("I am  %s, My age is %d, and I scored  %.2f percent in board exam" %(a,b,c))
+
+
+#Python Format Condition
+a ="Bhavya Shree"
+b =17
+c =87.256
+print("I am  {}, My age is {}, and I scored {} percent in my board exam".format(a,b,c))
+
+a ="AKSHEY"
+b =17
+c =85.245
+print(f"I scored {c} percent in board exam, I am  {a}, My age is {b}")
+
